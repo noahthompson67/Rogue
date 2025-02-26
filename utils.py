ae024514b0529c64639c3c79896477702e1101d7
nthompson1771 <35502667+nthompson1771@users.noreply.github.com>
2025-02-25 20:36:57 -0500

    Added vertical flip options for get_sprite

--- utils.py
+++ utils.py
@@ -9,12 +9,14 @@ def tint(surf, tint_color):
     surf.fill(tint_color[0:3] + (0,), None, pygame.BLEND_RGBA_ADD)
     return surf
 
-def get_sprite(sheet, location, width, height, scale, flip=False):
+def get_sprite(sheet, location, width, height, scale, flip_h=False, flip_v=False):
     img = pygame.Surface((width, height), pygame.SRCALPHA)
     if sheet is not None:
         img.blit(sheet, (0, 0), (location[0], location[1], width, height))
-    if flip:
+    if flip_h:
         img = pygame.transform.flip(img, True, False)
+    if flip_v:
+        img = pygame.transform.flip(img, False, True)
     img = pygame.transform.scale(img, (width * scale, height * scale))
     return img
 
